8b907c005a4947adf37304cd14e0e88ccee7e326
Xavier Amatriain <xavier.amatriain@gmail.com>
2024-01-17 16:45:44 -0800

    Add files via upload

---
+++ chatbotscripts.js
@@ -0,0 +1,16 @@
+document.getElementById('closeChatbot').addEventListener('click', function() {
+    var chatbot = document.getElementById('chatbot');
+    if (chatbot.style.display === 'none') {
+        chatbot.style.display = 'block';
+        this.textContent = 'X';
+        // Position for the open chatbot (inside the iframe)
+        this.style.right = 'calc(20px)'; 
+        this.style.bottom = 'calc(520px  - 30px)';
+    } else {
+        chatbot.style.display = 'none';
+        this.textContent = 'Open Chatbot';
+        // Position for the closed chatbot (bottom of the page)
+        this.style.right = '20px';
+        this.style.bottom = '20px';
+    }
+});
